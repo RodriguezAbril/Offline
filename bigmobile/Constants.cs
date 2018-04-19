fe3d89cc9269179623af27d8b45f7f83b26c8f02
abrilrodriguez <abril@octopus.ru>
2018-04-19 18:58:43 -0500

    mi primer commit
    xsvfcdxfcv
    
    file:   UWP/Assets/Square44x44Logo.targetsize-24_altform-unplated.png
    
    sxadsdasdas
    csdfsfsdf

---
+++ bigmobile/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace bigmobile
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://bigmobile.azurewebsites.net";
+	}
+}
+
